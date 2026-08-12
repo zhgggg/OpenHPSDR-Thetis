ea8b37e3c3c709b1b1e569f8d78259409e17a441
zhg213 <4496550@qq.com>
2026-08-13 01:47:02 +0800

    fix: guard Splash progress paint against zero-size rectangle (crash on startup)

--- Project Files/Source/Console/splash.cs
+++ Project Files/Source/Console/splash.cs
@@ -446,7 +446,8 @@ namespace Thetis
 			System.Windows.Forms.PaintEventArgs e)
 		{
 			if( m_bFirstLaunch == false && e.ClipRectangle.Width > 0 
-				&& m_iActualTicks > 1 )
+				&& m_iActualTicks > 1 
+				&& m_rProgress.Width > 0 && m_rProgress.Height > 0 )
 			{
 				LinearGradientBrush brBackground = 
 					new LinearGradientBrush(m_rProgress, 
